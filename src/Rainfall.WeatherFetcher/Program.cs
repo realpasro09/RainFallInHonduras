1d5ac488a0c48ccf085ac4298fbec67e3e5fe11c
Rene A. Rosa <realpasro09@hotmail.com>
2013-05-07 10:53:30 -0600

    Adding mapping of City entity in AlmanacDayOverride

--- src/Rainfall.WeatherFetcher/Program.cs
+++ src/Rainfall.WeatherFetcher/Program.cs
@@ -9,6 +9,7 @@ using FluentNHibernate.Cfg.Db;
 using NHibernate;
 using NHibernate.Linq;
 using Rainfall.Domain;
+using Rainfall.Domain.Entities;
 using Rainfall.Integration;
 using Rainfall.WeatherFetcher.APIClasses;
 
@@ -23,6 +24,7 @@ namespace Rainfall.WeatherFetcher
             ISessionFactory sessionFactory = new SessionFactoryBuilder(new MappingScheme(), databaseConfiguration).Build();
 
             ISession session = sessionFactory.OpenSession();
+            
             var weatherData = new WorldWeatherOnline(session);
             
             weatherData.SaveDataFromPast();
@@ -30,68 +32,6 @@ namespace Rainfall.WeatherFetcher
 
             session.Close();
             sessionFactory.Close();
-
-            #region Sample
-
-            /*
-                        using (var sess = sessionFactory.OpenSession())
-                        {
-                            using (IDbCommand cmd = sess.Connection.CreateCommand())
-                            {
-
-                                var weatherCondition = new AlmanacDay
-                                    {
-                                        Date = DateTime.Now,
-                                        AlmanacHourly = new AlmanacHourly[]
-                                            {}
-                                    };
-                                sess.Save(weatherCondition);
-                                var almanacHourlies = new AlmanacHourly[]
-                                    {
-                                        new AlmanacHourly
-                                            {
-                                                Date = DateTime.Now,
-                                                Time = 0,
-                                                Precipitation = 0.1,
-                                                TempHigh = 40,
-                                                TempLow = 32
-                                            },
-                                        new AlmanacHourly
-                                            {
-                                                Date = DateTime.Now,
-                                                Time = 0,
-                                                Precipitation = 0.0,
-                                                TempHigh = 42,
-                                                TempLow = 35
-                                            },
-                                        new AlmanacHourly
-                                            {
-                                                Date = DateTime.Now,
-                                                Time = 0,
-                                                Precipitation = 3.2,
-                                                TempHigh = 32,
-                                                TempLow = 25
-                                            }
-                                    };
-                    
-
-                                foreach (var almanacHourly in almanacHourlies)
-                                {
-                                    almanacHourly.AlmanacDayId = weatherCondition.Id;
-                                    sess.Save(almanacHourly);
-                                }
-                                weatherCondition.AlmanacHourly = almanacHourlies;
-                    
-                                var prec = weatherCondition.Get24HrsPrecipitation();
-                                var maxTemp = weatherCondition.Get24HrsTempHigh();
-                                var minTemp = weatherCondition.Get24HrsTempLow();
-
-                                var getData = sess.Query<AlmanacDay>().Where(x=>x.Date.Date==DateTime.Now);
-             
-                            }*/
-
-            #endregion
-
         }
     }
 }
