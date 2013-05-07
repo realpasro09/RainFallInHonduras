3fc483aa14af4442d1b576419224068ac9303d2e
Rene A. Rosa <realpasro09@hotmail.com>
2013-05-07 08:26:54 -0600

    Adding mapping of RainfallDataGridItemModel

--- src/Rainfall.Web/ConfigureAutomapper.cs
+++ src/Rainfall.Web/ConfigureAutomapper.cs
@@ -1,6 +1,8 @@
 using System;
 using AutoMapper;
 using Autofac;
+using Rainfall.Domain.Entities;
+using Rainfall.Web.Models;
 
 namespace Rainfall.Web
 {
@@ -13,8 +15,13 @@ namespace Rainfall.Web
             get { return container =>
                              {
                                  container.RegisterInstance(Mapper.Engine).As<IMappingEngine>();
-
-
+                                 Mapper.CreateMap<AlmanacDay, AlmanacDayGridItemModel>()
+                                       .ForMember(x => x.AlmanacDayId, o => o.MapFrom(x => x.Id))
+                                       .ForMember(x => x.TempHigh, o => o.MapFrom(x => x.Get24HrsTempHigh()))
+                                       .ForMember(x => x.TempLow, o => o.MapFrom(x => x.Get24HrsTempLow()))
+                                       .ForMember(x => x.Precipitation, o => o.MapFrom(x => x.Get24HrsPrecipitation()))
+                                       .ForMember(x => x.Date, o => o.MapFrom(x => x.Date.ToShortDateString()))
+                                       .ForMember(x => x.City, o => o.MapFrom(x => x.City.Name));
                              }; }
         }
 
