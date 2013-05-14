ae0cf39b3fc595c0be0d0aa61bd3e68df0a5afb7
Rene A. Rosa <realpasro09@hotmail.com>
2013-05-14 13:29:37 -0600

    deleting merged history files

--- src/Rainfall.Web/Models/IAlmanacDayGridSummaryModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Rainfall.Web.Models
-{
-    public interface IAlmanacDayGridSummaryModel
-    {
-        IEnumerable<AlmanacDayGridItemModel> AlmanacDays { get; set; }
-    }
-}
